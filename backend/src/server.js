e622cee67daac334de17f89defe9a7dac0cde07a
darshankok7 <darshankok7@gmail.com>
2025-09-18 18:27:09 +0530

    small

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,10 +22,10 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (_, res) => {
+  app.get("*", (_, res) => { 
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
-}
+};
 
 app.listen(PORT, () => {
   console.log("Server is running on port: " + PORT);
